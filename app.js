70ad66556cf59d1ce661707efebee3814e066f4e
SAURABH GUPTA <42804047+saurabhprasadsah@users.noreply.github.com>
2022-01-25 16:55:40 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const bodyParser = require("body-parser");
+const mysql = require("mysql");
+
+require("dotenv").config();
+
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+//parsing middlewares
+//parse application---////
+app.use(bodyParser.urlencoded({extended: false}));
+
+//parse application/json data//
+app.use(bodyParser.json());
+
+app.use(express.static('public'));
+
+//Templating engine
+
+//app.engine('hbs', exphbs({extname: '.hbs'}));
+
+//app.set('view engine','hbs');
+const handlebars = exphbs.create({ extname: '.hbs',});
+app.engine('.hbs', handlebars.engine);
+app.set('view engine', '.hbs');
+
+
+//Router
+app.get('', (req,res) => {
+   res.render('home');
+});
+app.listen(port, ()=> console.log(`Listining on port ${port}`));
